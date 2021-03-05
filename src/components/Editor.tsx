92dd0636f73b5669ead8d36d65bdb93aab7478ae
Priyath Gregory <priyath.gregory@syscolabs.com>
2021-03-05 09:40:33 +0530

    fix: fixed container layout

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -22,44 +22,49 @@ class Editor extends Component<Props, EditorState> {
         return (
             <div className="App">
 
-                <div className="document-editor">
-                    <h2>CKEditor 5 using a custom build - decoupled editor</h2>
-                    <div className="document-editor__toolbar"></div>
-                    <div className="document-editor__editable-container">
-                        <CKEditor
-                            onReady={ (editor: any) => {
-                                console.log( 'Editor is ready to use!', editor );
+                <div className="main-container">
+                    <div className="document-editor">
+                        <h2>CKEditor 5 using a custom build - decoupled editor</h2>
+                        <div className="document-editor__toolbar"></div>
+                        <div className="document-editor__editable-container">
+                            <CKEditor
+                                onReady={ (editor: any) => {
+                                    console.log( 'Editor is ready to use!', editor );
 
-                                (window as any).editor = editor;
+                                    (window as any).editor = editor;
 
-                                // properly position the toolbar
-                                const toolbarContainer = document.querySelector( '.document-editor__toolbar' );
-                                toolbarContainer!.appendChild( editor.ui.view.toolbar.element );
+                                    // properly position the toolbar
+                                    const toolbarContainer = document.querySelector( '.document-editor__toolbar' );
+                                    toolbarContainer!.appendChild( editor.ui.view.toolbar.element );
 
-                            } }
-                            onError={ ( { willEditorRestart }: {willEditorRestart: any} ) => {
-                                // If the editor is restarted, the toolbar element will be created once again.
-                                // The `onReady` callback will be called again and the new toolbar will be added.
-                                // This is why you need to remove the older toolbar.
-                                if ( willEditorRestart ) {
-                                    (window as any).editor.ui.view.toolbar.element.remove();
+                                } }
+                                onError={ ( { willEditorRestart }: {willEditorRestart: any} ) => {
+                                    // If the editor is restarted, the toolbar element will be created once again.
+                                    // The `onReady` callback will be called again and the new toolbar will be added.
+                                    // This is why you need to remove the older toolbar.
+                                    if ( willEditorRestart ) {
+                                        (window as any).editor.ui.view.toolbar.element.remove();
+                                    }
+                                } }
+                                onChange={ ( event: any, editor: any ) => {
+                                    const data = editor.getData({ pagination: true });
+                                    console.log( { event, editor, data } )
                                 }
-                            } }
-                            onChange={ ( event: any, editor: any ) => {
-                                const data = editor.getData({ pagination: true });
-                                console.log( { event, editor, data } )
-                            }
-                            }
-                            editor={ DecoupledEditor }
-                            data="<p>Hello from CKEditor 5's decoupled editor!</p>"
-                            config={ editorConfiguration }
-                        />
+                                }
+                                editor={ DecoupledEditor }
+                                data="<p>Hello from CKEditor 5's decoupled editor!</p>"
+                                config={ editorConfiguration }
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <h2>Tract Information</h2>
+                        <ReactJson src={this.props.dataSource} onSelect={(select) => {
+                            (window as any).editor.execute( 'placeholder', { value: select.value} );
+                            (window as any).editor.editing.view.focus();
+                        }}/>
                     </div>
                 </div>
-                <ReactJson src={this.props.dataSource} onSelect={(select) => {
-                    (window as any).editor.execute( 'placeholder', { value: select.value} );
-                    (window as any).editor.editing.view.focus();
-                }}/>
             </div>
         );
     }
